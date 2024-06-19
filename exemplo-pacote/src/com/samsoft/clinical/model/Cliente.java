06bc794d90a6898817dbead5f648c20251c76424
medinoSam <samuelejosuetutorial@gmail.com>
2024-06-19 09:27:17 -0300

    Package exemplo

---
+++ exemplo-pacote/src/com/samsoft/clinical/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.samsoft.clinical.model;
+
+public class Cliente {
+    
+}
---
+++ exemplo-pacote/src/com/samsoft/clinical/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.samsoft.clinical.service;
+
+import com.samsoft.clinical.model.Exame;
+
+public class ExameService {
+    void salvarExame(Exame exame){
+        
+    }
+}

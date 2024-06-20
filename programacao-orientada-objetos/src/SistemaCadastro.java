4317827a9809d5a503660761e5a76699ffe8fd47
medinoSam <samuelejosuetutorial@gmail.com>
2024-06-20 07:15:32 -0300

    Construtores

---
+++ programacao-orientada-objetos/src/SistemaCadastro.java
@@ -0,0 +1,9 @@
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        
+        Pessoa alguem = new Pessoa("Samisson", "123456");
+        
+        alguem.setEndereco("rua jose");
+        System.out.println(alguem.getNome() + " - " + alguem.getCpf() + " - " + alguem.getEndereco());
+    }
+}
---
+++ programacao-orientada-objetos/src/escola/Escola.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno samuel = new Aluno();
+
+        samuel.setNome("Samuel Medino");
+        samuel.setIdade(19);
+        System.out.println("o aluno " + samuel.getNome() + " possui " + samuel.getIdade() + " anos ");
+    }
+}

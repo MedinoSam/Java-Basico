b5dc15ab336e5ae6145a786c0cee133f9c835748
medinoSam <samuelejosuetutorial@gmail.com>
2024-06-25 08:37:27 -0300

    Operacoes basicas com list

---
+++ collections/src/list/CarrinhoDeCompras.java
@@ -0,0 +1,58 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    
+    private List<Item> carrinhoList;
+
+    public CarrinhoDeCompras(){
+        this.carrinhoList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        carrinhoList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensRemover = new ArrayList<>();
+
+        for(Item i : carrinhoList){
+            if(i.getNome().equalsIgnoreCase(nome)){
+                itensRemover.add(i);
+            }
+        }
+        carrinhoList.removeAll(itensRemover);
+    }
+
+    public double calcularValorTotal(){
+        int quantidadeList;
+        double precoList;
+        double total = 0;
+        for(Item i : carrinhoList){
+            quantidadeList = i.getQuantidade();
+            precoList = i.getPreco();
+            total += precoList*quantidadeList;
+            }
+            return total;
+        }
+
+    public void exibirItens(){
+        System.out.println(carrinhoList);
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras james = new CarrinhoDeCompras();
+        james.exibirItens();
+        james.adicionarItem("Cuscuz", 4, 10);
+        james.adicionarItem("Cuscuz", 4, 10);
+        james.adicionarItem("bolacha", 65, 10);
+        james.removerItem("cuscuz");
+        james.exibirItens();
+        System.out.println("valor total: " + james.calcularValorTotal());
+    }
+}
+
+
+
---
+++ collections/src/list/ListaTarefa.java
@@ -0,0 +1,52 @@
+package list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa(){
+        this.tarefaList = new ArrayList<>();
+        
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        //cria-se uma lista de tarefas para remover dentro da lista de tarefas
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        //percorre e compara os elementos dentro da lista de tarefas com o elemento passado como parametro
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                //se for igual, adiciona na lista de tarefas a remover
+                tarefasParaRemover.add(t);
+            }
+        }
+        //utiliza a lista de tarefas a remover como collections da funcao removeAll
+        tarefaList.removeAll(tarefasParaRemover);        
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+        
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa lt = new ListaTarefa();
+        System.out.println("numero total de tarefas: " + lt.obterNumeroTotalTarefas());
+        lt.adicionarTarefa("comer");
+        lt.adicionarTarefa("comer");
+        lt.adicionarTarefa("andar");
+        System.out.println("numero total de tarefas: " + lt.obterNumeroTotalTarefas());
+        lt.removerTarefa("comer");
+        lt.obterDescricoesTarefas();
+    }
+
+}

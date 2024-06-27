5c1332657cbd77baeaf1badbf29a568a39708c2c
medinoSam <samuelejosuetutorial@gmail.com>
2024-06-27 10:25:02 -0300

    pesquisa set

---
+++ collections/src/set/pesquisa/Tarefa.java
@@ -0,0 +1,73 @@
+package set.pesquisa;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+
+    public Tarefa(String descricao, boolean concluida) {
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public boolean getConcluida() {
+        return concluida;
+    }
+
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nTarefa [descricao=" + descricao + " concluida=" + concluida + "]\n";
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + (concluida ? 1231 : 1237);
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        if (concluida != other.concluida)
+            return false;
+        return true;
+    }
+
+    
+
+    
+    
+}

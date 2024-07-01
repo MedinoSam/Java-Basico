93c6682ccf011f5c5a76551fb4cf9e4062ba38c4
medinoSam <samuelejosuetutorial@gmail.com>
2024-07-01 20:24:45 -0300

    desafio quatro

--- Stream-api/src/desafios/DesafioQuatro.java
+++ Stream-api/src/desafios/DesafioQuatro.java
@@ -7,6 +7,6 @@ public class DesafioQuatro {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        numeros.stream().filter(n -> n % 2 != 0).
+        numeros.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
     }
 }

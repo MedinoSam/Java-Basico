beabcb706ab64885dd6f9b8809a427515eddf58c
medinoSam <samuelejosuetutorial@gmail.com>
2024-07-01 19:48:50 -0300

    Stream API examples

---
+++ Stream-api/src/BinaryOperatorExample.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 8, 14, 18);
+
+        int resultado = numeros.stream().reduce(0, (n1, n2) -> n1+n2);
+
+        System.out.println("A soma total eh: "+ resultado);
+    }
+}

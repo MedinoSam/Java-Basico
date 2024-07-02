4f378171e955b8ca69a9b24b0d29a5e00fc55063
medinoSam <samuelejosuetutorial@gmail.com>
2024-07-01 21:48:51 -0300

    desafio doze

---
+++ Stream-api/src/desafios/DesafioDoze.java
@@ -0,0 +1,14 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioDoze {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int produto = numeros.stream().reduce(1, (a,b) -> a*b);
+
+        System.out.println("O produto dos elementos eh: " + produto);
+    }
+}

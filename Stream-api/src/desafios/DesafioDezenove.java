00a322b897210a04578c72734b8f57d1cda862e2
medinoSam <samuelejosuetutorial@gmail.com>
2024-07-02 20:51:52 -0300

    desafio dezenove

---
+++ Stream-api/src/desafios/DesafioDezenove.java
@@ -0,0 +1,13 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioDezenove {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int soma = numeros.stream().filter(n -> n%3 == 0 && n % 5 == 0).reduce(0, Integer::sum); 
+        System.out.println("Soma dos numeros divisiveis por 3 e por 5: "+ soma);
+    }
+}

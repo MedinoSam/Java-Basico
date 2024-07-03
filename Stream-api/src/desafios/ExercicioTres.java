49fea093964e3161981476118561ab77334f8024
medinoSam <samuelejosuetutorial@gmail.com>
2024-07-03 08:27:24 -0300

    exercicio tres

---
+++ Stream-api/src/desafios/ExercicioTres.java
@@ -0,0 +1,50 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class ExercicioTres {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoInicial = scanner.nextDouble();
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        double valorDeposito = scanner.nextDouble();
+        conta.depositar(valorDeposito);
+
+        double valorSaque = scanner.nextDouble();
+        conta.sacar(valorSaque);
+
+        scanner.close();
+    }
+}
+
+class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Deposito feito.");
+        imprimirSaldo();
+    }
+
+    public void sacar(double valor) {
+        if(saldo >= valor){
+            saldo -= valor;
+            System.out.println("Saque feito.");
+        }
+        else{
+            System.out.println("Saldo insuficiente. Saque não realizado.");
+        }
+        imprimirSaldo();
+    }
+    
+    private void imprimirSaldo() {
+        System.out.printf("Saldo atual: %.1f\n", this.saldo);
+    }
+
+}
